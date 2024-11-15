b8f39c81213b72b3e94d58a7b649f74de645ae96
Mikita Saladukha <malkomus@gmail.com>
2024-11-15 12:15:40 +0300

    7 rewards. movingFront2 bug fixed

--- lines.sh
+++ lines.sh
@@ -312,6 +312,20 @@ function movingFront2() {
     # x=0.65 - врезается, 0.55- слишком медленно
 
   ros2 topic pub --once /cmd_vel geometry_msgs/Twist '{linear:  {x: 0.25, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+  restart_lag_counter=0
+  i=$(ros2 topic echo --once /scan -f)
+  while [ "$i" = "Waiting for at least 1 matching subscription(s)..." ]; do
+    i=$(ros2 topic echo --once /scan -f)
+    sleep 1
+    echo "restart countdown: "$((25-$restart_lag_counter))
+    restart_lag_counter=$(($restart_lag_counter+1))
+    if [ "$restart_lag_counter" -ge "25" ]
+    then
+      restart="True"
+      echo "restarting"
+      return
+    fi
+  done
   distanceAngle=($(python3 getDistanceFromAngle.py $i "0"))
   tempDif=($(python3 diffF1_F2.py $distanceAngle $slow_down_distance))
   bigger0=($(python3 biggerThanZero.py $tempDif))
